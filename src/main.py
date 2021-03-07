824fd7c356212ecc1df55f776e8e951daa4a847d
Bart Cramer <bart@klaredata.nl>
2021-03-07 21:59:49 +0100

    Added wind summaries and first leg true bearing to races

--- src/main.py
+++ src/main.py
@@ -20,9 +20,17 @@ def main():
     #uploader = RegattaUploader()
     #uploader.upload(path, conn, cursor)
 
-    path = "C:/data/powertracks/hwcs2020-round1/regattas/HWCS 2020 Round 1 - 49er/races.json"
     uploader = RaceUploader()
-    uploader.upload_races(path, conn, cursor)
+
+    #path = "C:/data/powertracks/hwcs2020-round1/regattas/HWCS 2020 Round 1 - 49er/races.json"
+    #dict_short_name_to_id = uploader.upload_races(path, conn, cursor)
+
+    #path = "C:/data/powertracks/hwcs2020-round1/regattas/HWCS 2020 Round 1 - 49er/windsummary.json"
+    #uploader.upload_windsummary(path, dict_short_name_to_id, conn, cursor)
+
+    path = "C:/data/powertracks/hwcs2020-round1/regattas/HWCS 2020 Round 1 - 49er/races/M Medal (49ER)/firstlegbearing.json"
+    race_id = "9c45be60-ad9f-0137-131d-06773f917276"
+    uploader.upload_first_leg_bearing(path, race_id, conn, cursor)
 
     cursor.close()
     conn.close()
--- src/uploaders/race_uploader.py
+++ src/uploaders/race_uploader.py
@@ -6,23 +6,55 @@ class RaceUploader:
         pass
 
     def upload_races(self, json_path, conn, cursor) : 
+        dict_short_name_to_id = {}
+
         with open(json_path) as json_file_object : 
             json_object = json.load(json_file_object)
-    
+            print(json_object)
+
             list_to_upload = []
             regatta_id = json_object['regatta']
             races_list = json_object['races']
             for record in races_list : 
-                list_to_upload.append((record['id'], regatta_id, record['name']))
+                name = record['name']
+                short_name = name.split(" ")[0]
+                list_to_upload.append((record['id'], regatta_id, name, short_name))
+                dict_short_name_to_id[short_name] = record['id']
 
-            query = "INSERT INTO powertracks.races(race_id, regatta_id, race_name) VALUES (?,?,?)"
+            query = "INSERT INTO powertracks.races(race_id, regatta_id, race_name, race_short_name) VALUES (?,?,?,?)"
 
             cursor.executemany(query, list_to_upload)
             cursor.commit()
+        
+        return dict_short_name_to_id
 
 
-    def upload_windsummary(self, json_path, conn, cursor) : 
-        pass
+    def upload_windsummary(self, json_path, dict_short_name_to_id, conn, cursor) : 
+        with open(json_path) as json_file_object : 
+            json_object = json.load(json_file_object)
+            
+            list_to_upload = []
+            for record in json_object : 
+                short_name = record['racecolumn']
+                race_id = dict_short_name_to_id[short_name]
+                fleet = record['fleet']
+                lo = record['trueLowerboundWindInKnots']
+                hi = record['trueUppwerboundWindInKnots']
+                avg = record['trueWindDirectionInDegrees']
 
-    def upload_first_leg_bearing(self, json_path, conn, cursor) : 
-        pass
+                list_to_upload.append((fleet, lo, hi, avg, race_id))
+
+            query = "UPDATE powertracks.races set fleet = ?, min_wind_speed_kts = ?, max_wind_speed_kts = ?, avg_wind_dir_deg = ? WHERE race_id = ?"
+            
+            cursor.executemany(query, list_to_upload)
+            cursor.commit()
+
+    def upload_first_leg_bearing(self, json_path, race_id, conn, cursor) : 
+        with open(json_path) as json_file_object : 
+            json_object = json.load(json_file_object)
+            deg = json_object['truebearingdegrees']
+
+            query = "UPDATE powertracks.races set first_leg_bearing_deg = ? WHERE race_id = ?"
+
+            cursor.execute(query, (deg, race_id))
+            cursor.commit()
