c22856c4822a7b571e82712a8f92de99a66c7b64
Bart Cramer <bart@klaredata.nl>
2021-03-11 17:24:07 +0100

    Find begin-ms and end-ms of each comp_leg in markpassings

--- src/uploaders/leg_uploader.py
+++ src/uploaders/leg_uploader.py
@@ -7,7 +7,7 @@ class LegUploader:
         self.dict_leg_id = {}        # (race_id, leg_nr) -> leg_id
         self.dict_leg_comp_id = {}   # (race_id, leg_nr, comp_id) -> leg_comp_id
 
-    def upload(self, json_path, race_id, conn, cursor) : 
+    def upload_legs(self, json_path, race_id, conn, cursor) : 
 
         with open(json_path) as json_file_object : 
             json_object = json.load(json_file_object)
@@ -49,7 +49,7 @@ class LegUploader:
                 comp_leg_id = str(uuid.uuid4())
                 self.dict_leg_comp_id[(race_id, leg_nr, comp_id)] = comp_leg_id
 
-                comp_leg_list_to_upload.append((leg_id, comp_id, \
+                comp_leg_list_to_upload.append((comp_leg_id, leg_id, comp_id, \
                     comp_record['distanceTraveled-m'], comp_record['averageSOG-kts'], \
                     comp_record['tacks'], comp_record['jibes'], comp_record['penaltyCircles'], comp_record['rank'], \
                     comp_record['gapToLeader-s'], comp_record['gapToLeader-m'], \
@@ -64,10 +64,36 @@ class LegUploader:
         cursor.executemany(query, leg_list_to_upload)
         cursor.commit()
 
-        query = "INSERT INTO powertracks.comp_leg(leg_id, comp_id, \
+        query = "INSERT INTO powertracks.comp_leg(comp_leg_id, leg_id, comp_id, \
             distance_traveled_m, average_sog_kts, tacks, jibes, penalty_circles, rank, \
             gap_to_leader_s, gap_to_leader_m, started, finished) \
-            VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
+            VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
         cursor.executemany(query, comp_leg_list_to_upload)
+        cursor.commit()
+    
+    def upload_markpassings(self, json_path, race_id, conn, cursor) :
+
+        with open(json_path) as json_file_object : 
+            json_object = json.load(json_file_object)
+
+        list_to_upload = []
+        for comp_record in json_object['bycompetitor'] :
+            comp_id = comp_record['competitor']['id']
+            mp_list = comp_record['markpassings']
+
+            for mp_record in mp_list :
+                ms = mp_record['timeasmillis']
+                leg_nr = mp_record['zeroBasedWaypointIndex']
+
+                if leg_nr > 0 : 
+                    list_to_upload[-1][1] = ms
+                if leg_nr < len(mp_list) - 1 : 
+                    comp_leg_id = self.dict_leg_comp_id[(race_id, leg_nr, comp_id)]
+                    list_to_upload.append([ms, 0, comp_leg_id])
+        
+        query = "UPDATE powertracks.comp_leg \
+            SET start_ms = ?, end_ms = ? \
+            WHERE comp_leg_id = ?"
+        cursor.executemany(query, list_to_upload)
         cursor.commit()
   
